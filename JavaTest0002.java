e07834559bb61aac82857b4090ea326ff6c5d6b5
Dipanshu Kubde <77319700+Dipanshu-Kubde@users.noreply.github.com>
2026-03-14 11:31:53 +0000

    Added code for JavaTest0002

---
+++ JavaTest0002.java
@@ -0,0 +1,23 @@
+public class JavaTest0002 {
+
+    public static void main(String[] args) {
+        int rows = 5;
+
+        for (int i = 1; i <= rows; i++) {
+
+            for (int space = 1; space <= (rows - i) * 2; space++) {
+                System.out.print(" ");
+            }
+
+            for (int j = 1; j <= i; j++) {
+                System.out.print(j + " ");
+            }
+
+            for (int j = i - 1; j >= 1; j--) {
+                System.out.print(j + " ");
+            }
+
+            System.out.println();
+        }
+    }
+}
